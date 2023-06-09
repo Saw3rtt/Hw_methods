3a0f2ad613b4e6444b876af0b58f227f46f2aa84
Saw3rtt <sawertpra@gmail.com>
2023-06-09 22:15:58 +0300

    Задание 1-3

---
+++ src/Methods.java
@@ -0,0 +1,70 @@
+import java.time.LocalDate;
+
+public class Methods {
+    public static void checkLeapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+
+    }
+
+    public static void task1() {
+        int year = 2021;
+        checkLeapYear(year);
+    }
+
+    public static void checkOS(int clientOS, int yearOfIssue) {
+        int iOS = 0;
+        int androidOS = 1;
+        int checkYear = LocalDate.now().getYear();
+        if (clientOS == iOS && yearOfIssue < checkYear) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == iOS) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == androidOS && yearOfIssue < checkYear) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == androidOS) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("ОС не поддерживается.");
+        }
+    }
+
+    public static void task2() {
+        int yearOfIssue = 2015;
+        int clientOS = 0;
+        checkOS(clientOS, yearOfIssue);
+
+    }
+
+    public static void deliver(int deliveryDistance) {
+        int delivery = 1;
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется дней: " + delivery);
+        } else if (deliveryDistance <= 60) {
+            System.out.println("Потребуется дней: " + (delivery + 1));
+        } else if (deliveryDistance < 100) {
+            System.out.println("Потребуется дней: " + (delivery + 2));
+
+        } else {
+            System.out.println("Доставки нет.");
+        }
+    }
+
+    public static void task3() {
+        int deliveryDistance = 90;
+        deliver(deliveryDistance);
+
+    }
+
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+
+    }
+}
+
+
